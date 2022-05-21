afc1917182e56c5f42b44cdcc6de9e77468cd135
Zakeh1480 <zakeh0875@hotmail.com>
2022-05-21 00:54:14 -0300

    Database seeding

--- Backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
@@ -5,6 +5,7 @@ import com.devsuperior.dscatalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @Service
@@ -13,6 +14,8 @@ public class CategoryService {
     @Autowired
     private CategoryRepository categoryRepository;
 
+    //Garante que o método estará incluido na transação do banco ou irá fazer uma transação.
+    @Transactional
     public List<Category> findAll(){
         return categoryRepository.findAll();
     }
