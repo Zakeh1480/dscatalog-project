b3792398fe8154f0c4ba19656810d42b5d5e262c
Zakeh1480 <zakeh0875@hotmail.com>
2022-05-21 01:20:35 -0300

    DTO

--- Backend/src/main/java/com/devsuperior/dscatalog/controller/CategoryController.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/controller/CategoryController.java
@@ -1,6 +1,6 @@
 package com.devsuperior.dscatalog.controller;
 
-import com.devsuperior.dscatalog.entity.Category;
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.service.CategoryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +19,8 @@ public class CategoryController {
 
     //Sempre que usar o List dentro do ResponseEntity, LEMBRE-SE de colocar as chaves "<>" e dentro o nome da classe usada.
     @GetMapping
-    public ResponseEntity<List<Category>> findAll() {
-        List<Category> categories = categoryService.findAll();
+    public ResponseEntity<List<CategoryDTO>> findAll() {
+        List<CategoryDTO> categories = categoryService.findAll();
         return ResponseEntity.status(200).body(categories);
     }
 }
--- Backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/service/CategoryService.java
@@ -1,12 +1,15 @@
 package com.devsuperior.dscatalog.service;
 
+import com.devsuperior.dscatalog.DTO.CategoryDTO;
 import com.devsuperior.dscatalog.entity.Category;
 import com.devsuperior.dscatalog.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CategoryService {
@@ -16,7 +19,17 @@ public class CategoryService {
 
     //Garante que o método estará incluido na transação do banco ou irá fazer uma transação.
     @Transactional
-    public List<Category> findAll(){
-        return categoryRepository.findAll();
+    public List<CategoryDTO> findAll() {
+        List<Category> list = categoryRepository.findAll();
+
+//        Convertendo uma lista Category em uma lista CategoryDTO - Jeito resumido.
+        return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
+//        Convertendo uma lista Category em uma lista CategoryDTO - Jeito detalhado.
+//        List<CategoryDTO> categoryDTOList = new ArrayList<>();
+//
+//        for (Category cat : list){
+//            categoryDTOList.add(new CategoryDTO(cat));
+//        }
     }
 }
