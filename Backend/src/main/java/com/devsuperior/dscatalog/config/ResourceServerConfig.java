0c66bf11e3c2fa05cf650b68bdc795d56a1269c6
Zakeh1480 <zakeh0875@hotmail.com>
2022-07-06 11:59:38 -0300

    H2 security configuration

--- Backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
+++ Backend/src/main/java/com/devsuperior/dscatalog/config/ResourceServerConfig.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscatalog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -9,15 +10,20 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.R
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
+import java.util.Arrays;
+
 @Configuration
 @EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
+    private Environment env;
+
+    @Autowired
     private JwtTokenStore jwtTokenStore;
 
     //Configurando um vetor com as rotas e quais roles poderão ser acesso.
-    public static final String[] PUBLIC = {"/oauth/token"}; //Login -> Liberado a todos.
+    public static final String[] PUBLIC = {"/oauth/token", "/h2-console/**"}; //Login -> Liberado a todos.
     public static final String[] OPERATOR_OR_ADMIN = {"/products/**", "/categories/**"};
     public static final String[] ADMIN = {"/user/**"};
 
@@ -30,6 +36,12 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     //Definindo/Configurando as rotas, permissões e roles.
     @Override
     public void configure(HttpSecurity http) throws Exception {
+
+        //Liberando o acesso ao H2 para o ambiente de test
+        if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
+            http.headers().frameOptions().disable();
+        }
+
         http.authorizeRequests()
                 .antMatchers(PUBLIC).permitAll()
                 //Liberando as requisições do tipo GET para products e categories. -> Mas se fizer o login as outras funcionam... ?
